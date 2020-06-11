b21da471b3d1503a75e136e8999fc35d0cd46017
Sherlock_Watson <19302010069@fudan.edu.cn>
2020-06-11 18:05:37 +0800

    lab11

---
+++ lab11/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'Login');
+define('DBUSER', 'sherlockWatson');
+define('DBPASS', '001128Xy');
+define('DBCONNSTRING','mysql:host=localhost;dbname=Login');
+?>
